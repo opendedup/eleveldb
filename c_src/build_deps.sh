a5b9426776b71beaf46935bb675e75184dcae31a
Sam Silverberg <sam.silverberg@gmail.com>
2017-04-18 19:02:40 -0700

    Update build_deps.sh

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -56,7 +56,7 @@ case "$1" in
 
     get-deps)
         if [ ! -d leveldb ]; then
-            git clone git://github.com/basho/leveldb
+            git clone git://github.com/opendedup/leveldb
             (cd leveldb && git checkout $LEVELDB_VSN)
             if [ "$BASHO_EE" = "1" ]; then
                 (cd leveldb && git submodule update --init)
@@ -81,7 +81,7 @@ case "$1" in
         export LEVELDB_VSN="$LEVELDB_VSN"
 
         if [ ! -d leveldb ]; then
-            git clone git://github.com/basho/leveldb
+            git clone git://github.com/opendedup/leveldb
             (cd leveldb && git checkout $LEVELDB_VSN)
             if [ $BASHO_EE = "1" ]; then
                 (cd leveldb && git submodule update --init)
